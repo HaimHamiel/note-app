3e7e3ae6f0fcbe9cba4c84ce82ad33ae4fbb92fb
haimhamiel <haim.hamiel@gmail.com>
2022-12-01 16:51:14 +0200

    Add express server and routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-//Server
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Basic Application" });
+});
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
